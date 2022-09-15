e821e250306c46b670d2d19bc40cdb5fde8bafe9
jenoh <memint.contact@gmail.com>
2022-09-15 14:44:02 +0200

    Add response time, get domain name

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
 use checkssl::CheckSSL;
+use reqwest;
+use std::time::Instant;
 
-fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    match CheckSSL::from_domain(&pattern) {
+#[tokio::main]
+async fn main() {
+    /*
+     * HTTP
+     */
+    let url = std::env::args().nth(1).expect("no pattern given");
+    let start = Instant::now();
+    let result = reqwest::get(url).await;
+    let elapsed = start.elapsed();
+    println!("Response time: {:.2?}", elapsed);
+    let domain = result.unwrap().url().host().unwrap().to_string();
+    println!("Domain name: {:?}", domain);
+
+    /*
+     * Check SSL
+     */
+    match CheckSSL::from_domain(&domain) {
         Ok(certificate) => {
             println!("Is valid: {:?}", certificate.server.is_valid);
             println!("Expiration date: {:?}", certificate.server.not_after);
