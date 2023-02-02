dbe37e1e1e9b1ef7ab848a348d699bd525708309
jenoh <memint.contact@gmail.com>
2023-02-02 17:29:16 +0100

    Add status code

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,15 @@ async fn main() {
      * HTTP
      */
     let url = std::env::args().nth(1).expect("no pattern given");
+    println!("Url: {:?}", url);
     let start = Instant::now();
     let result = reqwest::get(url).await;
     let elapsed = start.elapsed();
     println!("Response time: {:.2?}", elapsed);
-    let domain = result.unwrap().url().host().unwrap().to_string();
+    let domain = result.as_ref().unwrap().url().host().unwrap().to_string();
     println!("Domain name: {:?}", domain);
+    let status = result.as_ref().unwrap().status().to_string();
+    println!("Status: {:?}", status);
 
     /*
      * Check SSL
